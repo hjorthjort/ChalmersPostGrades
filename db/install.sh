04425cd46f7a2c07abbe294902cf3f5b74b0ef60
hjorthjort <rikard.hjort@gmail.com>
2015-12-10 22:34:38 +0100

    Automate user creation

--- db/install.sh
+++ db/install.sh
@@ -18,16 +18,30 @@ printf "\n#Database directory\n$1\n" >> .gitignore
 printf "\n#Password file\npassword\n" >> .gitignore
 
 # Set postgresuser
-export PGUSER="$3"
+printf "Enter username: "
+read line
+user="$line"
+export PGUSER="$user"
 
-# Create the database
-initdb -D "$1" --username=dbadmin --pwfile=password
+# Get a password, store in variable $line, store in password file
+rm -f password
+printf "Enter password: "
+read -s line
+password="$line"
+printf "$password" > password
+chmod 600 password
+
+# Create database
+initdb -D "$1" --username="$user" --pwfile=password
 
 # Start the server and wait for it to start before proceeding (-w flag)
 pg_ctl -w -D "$1" -l "$1"/logfile start
 
+# Create the new user on the server
+createuser -s "$user"
+
 # Create new db
-createdb -U "$3" "$1"/"$2"
+createdb -U "$user" "$1"/"$2"
 
 # Initiate based on an init file
-psql -U "$3" --file=init_db.sql "$1"/"$2"
+psql -U "$user" --file=init_db.sql "$1"/"$2"
