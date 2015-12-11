5d299d1ca53e724419ccd8234b96a3caa2a9973d
hjorthjort <rikard.hjort@gmail.com>
2015-12-11 17:53:06 +0100

    Update Readme

--- README.md
+++ README.md
@@ -2,10 +2,10 @@ Chalmers Post(greSQL) Grades
 ======
 Postgres database of all grade statistics at Chalmers University of Technology since fall 2011.
 
-Warning!
---------
+Warning
+-------
 
-Don't install if you are already running a postgres database on your machine, or if you have a folder called data in the folder you are in. Follow instructions carefully.
+If there is an existing database running in the specified folder (first argument), it will be deleted.
 
 Initializing the database
 -------------------------
@@ -14,6 +14,8 @@ Run install.sh <folder-where-you-want-the-databast> <database-name>
 
 What did I just do?
 -------------------
+
+If there was an existing server running in the specified folder, you removed it and stopped it.
 
 1. You created the postgres environement, the cluster, the big shebang. And it is all in that folder you wanted
 1. You created a user with a password.
--- init_db.sql
+++ init_db.sql
@@ -56,3 +56,58 @@ CREATE OR REPLACE VIEW GradedExamsAverages AS (
         WHERE (course, date) IN (SELECT course, date FROM Over20TakersExams)
         GROUP BY course, course_name, date
 );
+
+
+-- The population used for our statisticla analysis
+CREATE OR REPLACE VIEW ThePopulation AS (
+    SELECT row_number() OVER () as row, GradedExamsAverages.* 
+    FROM GradedExamsAverages 
+        WHERE date > '2013-07-01'
+);
+
+-- The random sample of courses we use for our analysis
+CREATE OR REPLACE VIEW TheRandomSample AS (
+    SELECT *
+    FROM ThePopulation
+        WHERE 
+            row = 563 OR
+            row = 1223 OR
+            row = 1091 OR
+            row = 1282 OR
+            row = 875 OR
+            row = 603 OR
+            row = 1006 OR
+            row = 28 OR
+            row = 623 OR
+            row = 995 OR
+            row = 916 OR
+            row = 1283 OR
+            row = 1489 OR
+            row = 172 OR
+            row = 1304 OR
+            row = 588 OR
+            row = 1522 OR
+            row = 233 OR
+            row = 1526 OR
+            row = 338 OR
+            row = 764 OR
+            row = 341 OR
+            row = 253 OR
+            row = 1341 OR
+            row = 287 OR
+            row = 825 OR
+            row = 1004 OR
+            row = 251 OR
+            row = 1447 OR
+            row = 627
+        );
+
+-- The women
+CREATE OR REPLACE VIEW TheWomenSample AS (
+    SELECT * 
+    FROM ThePopulation
+        WHERE 
+            row = 903 OR
+            row = 1005 OR
+            row = 805
+);
